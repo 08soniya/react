a91a4b48e9c8625bc23e811394f04bcd5f1afccc
Soniya Agrawal <soniyaagrawal@Soniyas-MacBook-Air.local>
2024-05-09 18:53:01 +0530

    Week 1

---
+++ firstProject/src/App.jsx
@@ -0,0 +1,11 @@
+import Test from "./test"
+function App() {
+  return (
+    <>
+    <Test />
+    <h1>React learning by viet</h1>
+    </>
+  )
+}
+
+export default App
---
+++ firstProject/src/test.jsx
@@ -0,0 +1,7 @@
+function Test(){
+    return(
+        <h3>This is my test compenent</h3>
+    )
+}
+
+export default Test
